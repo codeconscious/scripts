e0c95317048d7e37123c9682a18e43db90486f01
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-05-12 10:46:52 +0900

    Add milestones

--- fsharp/days-since.fsx
+++ fsharp/days-since.fsx
@@ -1,72 +1,105 @@
-module IO =
-    open System.IO
-    let readFile fileName =
-        try
-            fileName
-            |> File.ReadAllText
-            |> Ok
-        with
-            | :? FileNotFoundException -> Error $"\"{fileName}\" was not found."
-            | e -> Error $"Unexpectedly could not read \"{fileName}\": {e.Message}"
+open System.IO
+let readFile fileName =
+    try
+        fileName
+        |> File.ReadAllText
+        |> Ok
+    with
+        | :? FileNotFoundException -> Error $"\"{fileName}\" was not found."
+        | e -> Error $"Unexpectedly could not read \"{fileName}\": {e.Message}"
 
-module Main =
-    open IO
-    open System
+open System
 
-    type ResultBuilder() =
-        member this.Bind(m, f) =
-            match m with
-            | Error e -> Error e
-            | Ok a -> f a
+type ResultBuilder() =
+    member this.Bind(m, f) =
+        match m with
+        | Error e -> Error e
+        | Ok a -> f a
 
-        member this.Return(x) =
-            Ok x
+    member this.Return(x) =
+        Ok x
 
-    let result = new ResultBuilder()
+let result = new ResultBuilder()
 
-    type Entry =
-        { Description : string
-          Date : DateOnly }
+type Entry =
+    { Description : string
+      Date : DateOnly
+      DayNumber : int }
 
-    let fileName =
-        fsi.CommandLineArgs
-        |> Array.toList
-        |> List.tail
-        |> List.head
+type Milestone =
+    { Date : DateOnly
+      DaysUntil : int
+      DaysOf : int }
 
-    let splitLines (text:string) =
-        text.Split('\n', StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
+type EntryWithMilestone =
+    { Entry : Entry
+      Milestone : Milestone }
 
-    let splitToPairs (text:string[]) =
-        text
-        |> Array.map (fun t -> t.Split(',', StringSplitOptions.TrimEntries))
-        |> Array.map (fun p -> (p[0], p[1]))
+let fileName =
+    fsi.CommandLineArgs
+    |> Array.toList
+    |> List.tail
+    |> List.head
 
-    // let convertToEntries (pairs:(string * string) array) =
-    //     let isDateValid (maybeDate:string) =
-    //         match maybeDate |> DateOnly.TryParse with
-    //         | (true, d) -> Ok d
-    //         | _ -> Error $"Invalid date: \"{maybeDate}\""
+let splitLines (text:string) =
+    text.Split('\n', StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
 
-    //     pairs
-    //     |> Array.partition (fun p -> (fst p) |> DateOnly.TryParse)
+let splitToPairs (text:string[]) =
+    text
+    |> Array.map (fun t -> t.Split(',', StringSplitOptions.TrimEntries))
+    |> Array.map (fun p -> (p[0], p[1]))
 
-    let pairs =
-        result {
-            let! text = readFile fileName
-            let lines = text |> splitLines
-            let pairs = lines |> splitToPairs
-            let withValidDates = pairs |> Array.filter (fun x ->
-                let isValid, _ = DateOnly.TryParse(snd x)
-                isValid)
-            printfn $"{withValidDates.Length} withValidDate(s) found"
-            let pairs =
-                withValidDates
-                |> Array.map (fun p -> {Description = fst p; Date = DateOnly.Parse(snd p)})
-            printfn $"{pairs.Length} pair(s) found"
-            return pairs
-        }
+// let convertToEntries (pairs:(string * string) array) =
+//     let isDateValid (maybeDate:string) =
+//         match maybeDate |> DateOnly.TryParse with
+//         | (true, d) -> Ok d
+//         | _ -> Error $"Invalid date: \"{maybeDate}\""
 
-    match pairs with
-    | Ok p -> p |> Array.iter (fun item -> printfn $"{item.Date.ToString()}: {item.Description}")
-    | Error e -> printfn $"ERROR: {e}"
+//     pairs
+//     |> Array.partition (fun p -> (fst p) |> DateOnly.TryParse)
+
+let calc (pairs:(string * string) array) =
+    let milestoneInterval = 1000
+    let today = DateOnly.FromDateTime(DateTime.Now)
+
+    let createEntry (pair:(string * string)) =
+        let parsedDate = DateOnly.Parse(snd pair)
+        let since = today.DayNumber - parsedDate.DayNumber + 1
+        { Description = fst pair; Date = parsedDate; DayNumber = since }
+
+    let createMilestone interval (entry:Entry) =
+        // DayCount = daysSince + _interval - (daysSince % _interval);
+        // DaysUntil = DayCount - daysSince;
+        // Date = DateOnly.FromDateTime(DateTime.Now.Date.AddDays(DaysUntil));
+        let daysSince = today.DayNumber - entry.Date.DayNumber + 1;
+        let daysOf = daysSince + interval - (daysSince % interval)
+        let daysUntil = daysOf - daysSince
+        let date = DateOnly.FromDateTime(DateTime.Now.Date.AddDays(daysUntil))
+        let milestone = { Date = date; DaysUntil = daysUntil; DaysOf = daysOf }
+        { Entry = entry; Milestone = milestone }
+
+    pairs
+    |> Array.map createEntry
+    |> Array.map (fun x -> x |> createMilestone milestoneInterval)
+
+let pairs =
+    result {
+        let! text = readFile fileName
+        let lines = text |> splitLines
+        let pairs = lines |> splitToPairs
+        let withValidDates = pairs |> Array.filter (fun x ->
+            let isValid, _ = DateOnly.TryParse(snd x)
+            isValid)
+        printfn $"{withValidDates.Length} withValidDate(s) found"
+        let entriesWithMilestones = withValidDates |> calc
+        // let pairs =
+        //     withValidDates
+        //     |> Array.map (fun p -> {Description = fst p; Date = DateOnly.Parse(snd p)})
+        //     |> Array.sortBy (fun p -> p.Date)
+        printfn $"{entriesWithMilestones.Length} entriesWithMilestone(s) found"
+        return entriesWithMilestones
+    }
+
+match pairs with
+| Ok p -> p |> Array.iter (fun item -> printfn $"{item.Entry.Date.ToString()}: {item.Entry.Description}")
+| Error e -> printfn $"ERROR: {e}"
