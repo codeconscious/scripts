84a5512d1518f36bc093a5796f1ecb9d2f5006b8
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-05-23 16:47:52 +0900

    Add CSV formatting

--- bash/last-commited.sh
+++ bash/last-commited.sh
@@ -17,5 +17,5 @@ for f in $(git ls-files "$DIRECTORY")
 do
     git --no-pager log --color -1 --date=short --pretty=format:'%C(cyan)%ai%Creset' -- "$f"
     # echo  " $f"; done | sort -r
-    echo  " $f"
+    echo  ", $f"
 done
