c00a2f3e592a9182a05a9f14d6f55709afba1ced
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-06-20 12:52:32 +0900

    Exit on error

--- bash/last-commited.sh
+++ bash/last-commited.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-# Passed a single directory, lists the dates each file in that directory were last committed in git.
+# Passed a single directory, lists the dates each file
+# in that directory was last committed in git.
 # TODO: Check for no directory passed in.
 
-[[ ! -a $(which git) ]] && echo "Git is missing." && exit
+[[ ! -a $(which git) ]] && echo "Git is missing." && exit 1
 
 DIRECTORY=$1
 if [ ! -d "$DIRECTORY" ]; then
   echo "\"$DIRECTORY\" does not exist."
+  exit 2
 fi
 
 # Adapted from https://stackoverflow.com/a/58330257/11767771:
