eb9ab3d81d1ea36992f2e927bc3b3694579fb065
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-07-02 17:41:21 +0900

    Use 'do' block form

--- ruby/list_public_method_names.rb
+++ ruby/list_public_method_names.rb
@@ -1,3 +1,6 @@
+# Summary: When passed a directory, prints a CSV-formatted list of
+# all of public methods defined in all Ruby files within that directory.
+
 if ARGV.count != 1
   puts "Enter a single directory containing .rb files"
   return
@@ -5,22 +8,46 @@ end
 
 dir = ARGV[0]
 
-files = Dir.entries(dir).filter { |fn| fn.end_with?('.rb') }.sort
-puts "Found #{files.count} files in \"#{dir}\"."
+unless Dir.exist?(dir)
+  puts "Directory does not exist."
+  return
+end
 
-Dir.chdir(dir) do
-  files.each do |file|
-    lines = []
+begin
+  files = Dir.glob("#{dir}/**/*.rb")
+rescue => ex
+  puts "Error: #{ex.Message}"
+  return
+end
 
-    # Gather only the method names in method definitions.
-    IO.foreach(file) do |line|
-      break if line.match? "^\s+private"
+if files.count.zero?
+  puts "No *.rb files found."
+  return
+end
 
-      method_name = /^\s+def (.+?)(?:; end|\(.*\))?$/.match line
-      lines << method_name.captures[0] if method_name
+puts "Directory,Filename,Line,Method"
+
+MethodDefinition = Struct.new(:dir, :filename, :line_number, :method_name)
+method_def_regex = /^\s+def ([\w_\?]+)/ # Method names are in capture group 0.
+
+files.each do |file|
+  line_number = 0
+  method_defs = []
+
+  IO.foreach(file) do |line|
+    line_number = line_number + 1
+    break if line.match? "^\s+?private"
+
+    if (method_name = method_def_regex.match line)
+      method_defs << MethodDefinition.new(
+                       File.dirname(file),
+                       File.basename(file),
+                       line_number,
+                       method_name.captures[0])
     end
+  end
 
-    puts "#{file}"
-    lines.each { |l| puts "  - #{l}" }
+  method_defs.each do |m|
+    puts "#{m.dir},#{m.filename},#{m.line_number},#{m.method_name}"
   end
 end
