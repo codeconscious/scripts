e803014dbcf4e48d40bd464288ccd664b876433b
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-09-06 17:49:11 +0900

    Hide hidden files

--- fsharp/RecursiveRandomRename.fsx
+++ fsharp/RecursiveRandomRename.fsx
@@ -1,27 +1,29 @@
 open System.IO
 
-type FileName = FileName of string
-type DirectoryName = DirectoryName of string
-
 type DirectoryItem =
-    | SubDirectory of DirectoryName
-    | File of FileName
+    | DirectoryName of string
+    | FileName of string
 
-let rec getAllFiles dir pattern =
-    seq { yield! Directory.EnumerateFiles(dir, pattern)
-          for d in Directory.EnumerateDirectories(dir) do
-              yield d
-              yield! getAllFiles d pattern }
+let rec getAllFiles dir pattern : seq<DirectoryItem> =
+    let isNotHidden (path:string) =
+        let attrs = File.GetAttributes(path)
+        not <| attrs.HasFlag(FileAttributes.Hidden)
 
-let isDir (path:string) =
-    let attr = File.GetAttributes(path)
-    attr.HasFlag(FileAttributes.Directory)
+    seq {
+        yield! Directory.EnumerateFiles(dir, pattern)
+               |> Seq.where (fun f -> isNotHidden f)
+               |> Seq.map FileName
+
+        for d in Directory.EnumerateDirectories(dir) do
+            yield DirectoryName d
+            yield! getAllFiles d pattern
+    }
 
 let renameFile oldName newName =
     try
         Ok <| File.Move(oldName, newName)
     with
-        | :? FileNotFoundException -> Error $"\"{oldName}\" was not found."
+        | :? FileNotFoundException -> Error $"File \"{oldName}\" was not found."
         | e -> Error $"Failure renaming \"{oldName}\" to \"{newName}\": {e.Message}"
 
 let renameDir oldName newName =
@@ -31,6 +33,11 @@ let renameDir oldName newName =
         | :? FileNotFoundException -> Error $"Directory \"{oldName}\" was not found."
         | e -> Error $"Failure renaming \"{oldName}\" to \"{newName}\": {e.Message}"
 
+let summarize dirItem =
+    match dirItem with
+    | DirectoryName d -> $"- {d}"
+    | FileName f -> $"  {f}"
+
 getAllFiles "/Users/jd/Downloads/generated_files/" "*"
-|> Seq.where (fun p -> Path.GetFileName(p)[0] <> '.')
-|> Seq.iter (fun p -> printfn "[%s] %s" (if isDir p then "D" else "F") p)
+// |> Seq.where (fun p -> Path.GetFileName(p)[0] <> '.')
+|> Seq.iter (fun p -> printfn "%s" (summarize p))
