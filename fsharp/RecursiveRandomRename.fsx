5eb261c859708a7873a6995aa0982a83dc6712da
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-09-07 10:02:20 +0900

    Improve isNotHidden; update text output

--- fsharp/RecursiveRandomRename.fsx
+++ fsharp/RecursiveRandomRename.fsx
@@ -5,10 +5,14 @@ type DirectoryItem =
     | DirectoryName of string
     | FileName of string
 
-let rec getAllFiles dir pattern : seq<DirectoryItem> =
-    let isNotHidden (path:string) =
-        let attrs = File.GetAttributes(path)
-        not <| attrs.HasFlag(FileAttributes.Hidden)
+let rec allDirectoryItems dir pattern : seq<DirectoryItem> =
+    let isNotHidden (path: string) =
+        match path with
+        | p when p[0] = '.'
+            -> false
+        | p ->
+            let attrs = File.GetAttributes(p)
+            not <| attrs.HasFlag(FileAttributes.Hidden)
 
     let items = seq {
         yield! Directory.EnumerateFiles(dir, pattern)
@@ -17,10 +21,11 @@ let rec getAllFiles dir pattern : seq<DirectoryItem> =
 
         for d in Directory.EnumerateDirectories(dir) do
             yield DirectoryName d
-            yield! getAllFiles d pattern
+            yield! allDirectoryItems d pattern
     }
 
-    // Reversing snsures files are renamed before their enclosing directory.
+    // Reversing ensures files are renamed before their enclosing directory,
+    // preventing renaming errors.
     items |> Seq.rev
 
 let rename path =
@@ -30,8 +35,9 @@ let rename path =
         try
             let dir = Path.GetDirectoryName(oldName)
             let ext = Path.GetExtension(oldName) // Includes initial period
-            let newName = Path.Combine(dir, $"{newGuid()}{ext}")
-            File.Move(oldName, newName)
+            let newName = $"{newGuid()}{ext}"
+            let newPath = Path.Combine(dir, newName)
+            File.Move(oldName, newPath)
             Ok $"Renamed file \"{oldName}\" to \"{newName}\"."
         with
             | :? FileNotFoundException -> Error $"File \"{oldName}\" was not found."
@@ -40,18 +46,19 @@ let rename path =
     let renameDir (oldName: string) =
         try
             let dir = Path.GetDirectoryName(oldName)
-            let newName = Path.Combine(dir, newGuid())
-            Directory.Move(oldName, newName)
+            let newName = newGuid()
+            let newPath = Path.Combine(dir, newName)
+            Directory.Move(oldName, newPath)
             Ok $"Renamed directory \"{oldName}\" to \"{newName}\"."
         with
             | :? FileNotFoundException -> Error $"Directory \"{oldName}\" was not found."
-            | e -> Error $"Failure renaming \"{oldName}\" to \"{newGuid}\": {e.Message}"
+            | e -> Error $"Failure renaming \"{oldName}\": {e.Message}"
 
     match path with
     | FileName f -> renameFile f
     | DirectoryName d -> renameDir d
 
-getAllFiles "/Users/jd/Downloads/generated_files/" "*"
+allDirectoryItems "/Users/jd/Downloads/generated_files/" "*"
 |> Seq.map (fun itemInDir -> rename itemInDir)
 |> Seq.iter (function
              | Ok s    -> printfn "[OK] %s" s
