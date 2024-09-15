a3c505f6e66ff85b017c1a1f92c5442144300bb0
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-09-15 18:09:50 +0900

    Handled out-of-range numbers

--- fsharp/API/IsEven.fsx
+++ fsharp/API/IsEven.fsx
@@ -1,4 +1,5 @@
 open System
+open System.Net
 open System.Net.Http
 open System.Text.Json
 open System.Text.Json.Serialization
@@ -9,26 +10,32 @@ type ApiResponse = {
     [<JsonPropertyName("iseven")>] IsEven: bool
 }
 
-let fetchApiData (url: string) : Async<Result<ApiResponse,string>> =
+let fetchApiData (number : int) : Async<Result<ApiResponse,string>> =
+    let url = $"https://api.isevenapi.xyz/api/iseven/{number}/"
+
     async {
         use client = new HttpClient()
         try
-            let! response = client.GetStringAsync(url) |> Async.AwaitTask
-            let options = JsonSerializerOptions(PropertyNamingPolicy = JsonNamingPolicy.CamelCase)
-            let data = JsonSerializer.Deserialize<ApiResponse>(response, options)
-            return Ok data
+            let! response = client.GetAsync(url) |> Async.AwaitTask
+            if response.StatusCode = HttpStatusCode.Unauthorized
+            then
+                return Error $"Unauthorized: {number} is out of range."
+            else
+                response.EnsureSuccessStatusCode () |> ignore
+                let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+                let options = JsonSerializerOptions(PropertyNamingPolicy = JsonNamingPolicy.CamelCase)
+                let data = JsonSerializer.Deserialize<ApiResponse>(content, options)
+                return Ok data
         with
         | :? HttpRequestException as ex ->
             return Error $"Request error: {ex.Message}"
         | ex ->
-            return Error $"An error occurred: {ex.Message}"
-    } // |> Async.StartAsTask
+            return Error ex.Message
+    }
 
-let number = Random().Next(1_000_000)
-// let number = 1000000
-let url = $"https://api.isevenapi.xyz/api/iseven/{number}/"
+let number = Random().Next(1_100_000) // Only up to 1 million (exclusive) is supported.
 
-let result = fetchApiData url |> Async.RunSynchronously
+let result = fetchApiData number |> Async.RunSynchronously
 match result with
 | Ok data -> printfn $"{number} = {data.IsEven}  |  {data.Ad}"
 | Error e -> printfn $"Error: {e}"
