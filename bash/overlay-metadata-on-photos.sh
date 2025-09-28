2a8ef01eccce3476edd50ecf4fc7d032da6a9924
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-09-28 12:35:17 +0900

    Add more metadata

--- bash/overlay-metadata-on-photos.sh
+++ bash/overlay-metadata-on-photos.sh
@@ -1,32 +1,64 @@
 #!/bin/bash
 input_image="$1"
+echo "▲ $input_image"
 
-# Ensure proper locale settings
+image_base_filename=$(basename -s .jpg "$input_image")
+dir_name=$(dirname "$input_image")
+parent_dir_name=$(dirname "$dir_name")
+
+xmp_file="${parent_dir_name}/${image_base_filename}.ORF.xmp"
+
+# Exit if no sidecar file found
+if [[ ! -f "$xmp_file" ]]; then
+    echo "No XMP sidecar file found at $xmp_file"
+    exit 1
+fi
+
+echo "Sidebar file: $xmp_file"
+
+# Locale settings
 export LANG=ja_JP.UTF-8
 export LC_ALL=ja_JP.UTF-8
 
-exif_title=$(exiftool -s3 -Title "$input_image")
-exif_description=$(exiftool -s3 -Description "$input_image")
-exif_datetime=$(exiftool -s3 -d "%Y年%m月%d日 %H時%M分" -DateTimeOriginal "$input_image")
+title=$(exiftool -XMP-dc:Title -b "$xmp_file")
+if [[ -n "$title" ]]; then
+    echo "Title: $title"
+else
+    echo "No title found"
+fi
 
-# Add text after `:-` to specify backup text if the value is missing.
-metadata_text="${exif_title:-}　　｜　　${exif_description:-}　　｜　　${exif_datetime}"
+description=$(exiftool -XMP-dc:Description -b "$xmp_file")
+if [[ -n "$description" ]]; then
+    echo "Description: $description"
+else
+    echo "No description found"
+fi
+
+
+datetime=$(exiftool -s3 -d "%Y年%m月%d日 %H時%M分" -DateTimeOriginal "$input_image")
+
+output_photo="${input_image}_annotated.jpg"
 
 magick "$input_image" \
-  -bordercolor white \
-  -border 20 \
-  -gravity South \
-  -background white \
-  -splice 0x50 \
-  -font KozMinPro \
-  -font KozMinPro-Medium \
-  -font "Hiragino Mincho Pro" \
-  -font "YuKyokasho Yoko" \
-  -font "Noto-Sans-CJK-JP" \
-  -font "Source-Han-Sans-JP" \
-  -font "Hiragino-Sans-GB" \
-  -font "/Library/Fonts/Arial Unicode.ttf" \
-  -pointsize 45 \
-  -fill black \
-  -annotate +0+10 "$metadata_text" \
-  "${input_image}_annotated.jpg"
+    -bordercolor white \
+    -border 40 \
+    -gravity South \
+    -font KozMinPro \
+    -font KozMinPro-Medium \
+    -font "Hiragino Mincho Pro" \
+    -font "YuKyokasho Yoko" \
+    -font "Noto-Sans-CJK-JP" \
+    -font "Source-Han-Sans-JP" \
+    -font "Hiragino-Sans-GB" \
+    -font "/Library/Fonts/Arial Unicode.ttf" \
+    -pointsize 85 \
+    -fill black \
+    -gravity Northwest \
+    -annotate +10+10 "$title" \
+    -gravity Northeast \
+    -annotate +10+10 "$datetime" \
+    -gravity South \
+    -annotate +0+10 "$description" \
+    "$output_photo"
+
+echo "Annotated photo saved as $output_photo"
