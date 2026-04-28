7a935806918332afb92ba3595babcc1cae0566d7
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-04-28 18:11:11 +0900

    Better result handling

--- fsharp/Enquoten.fsx
+++ fsharp/Enquoten.fsx
@@ -1,3 +1,5 @@
+#r "nuget: FsToolkit.ErrorHandling"
+
 (* Enquoten: An F# Script
 
    Summary: Reads a single text file and splits its lines to the maximum line length
@@ -5,55 +7,49 @@
             must be shorter than the line length limit. The script will try to split lines
             at spaces, but if none are found, it will cut the line at the line length limit.
 
-   Requirements: .NET 8 runtime (Untested on previous versions, though it might work)
+   Requirements: .NET 10 runtime (Untested on previous versions, though it might work)
 
    Usage: dotnet fsi <lineLengthLimit> <quotePrefix> <filePath>
           Sample: dotnet fsi 70 "> " file1.txt
 
    Note: This was created mainly for learning purposes and might not be very good. ^_^
 
-   Last significant update: May 4, 2024, to add computation expressions and allow custom prefixes.
+   Last significant updates:
+   - May 4, 2024, to add computation expressions and allow custom prefixes.
+   - Feb 13, 2026
 *)
 
 open System
 open System.IO
+open FsToolkit.ErrorHandling
 
 type Args<'T> =
     {
         /// The maximum length each line of text should be, including the prefix text.
         Limit: 'T
+
         /// A prefix (such as "> ") for each line.
         Prefix: string
+
         /// The name of the comma-separated input file.
         File: string
     }
 
 module ArgValidation =
-    type ResultBuilder() =
-        member this.Bind(m, f) =
-            match m with
-            | Error e -> Error e
-            | Ok a -> f a
-
-        member this.Return(x) =
-            Ok x
-
-    let result = ResultBuilder()
-
     let validateArgs =
         let rawArgs =
             fsi.CommandLineArgs
             |> Array.toList
             |> List.tail // The head contains the script filename.
 
-        let validateArgCount (args:string list) =
+        let validateArgCount (args: string list) =
             match args.Length with
             | l when l = 3 ->
                 Ok { Limit = args.Head; Prefix = args[1]; File = args[2] }
             | _ ->
                 Error "You must supply a maximum line length, a quote prefix (such as '> '), and one file path."
 
-        let validateLimit (args:Args<string>) =
+        let validateLimit (args: Args<string>) =
             match args with
             | { Args.Limit = l; Prefix = p; Args.File = f } ->
                 match (l |> Int32.TryParse) with
@@ -61,7 +57,7 @@ module ArgValidation =
                 | true, _ -> Error "Requested line length too short."
                 | _ -> Error "Maximum line length must be numeric."
 
-        let validatePrefix (args:Args<int>) =
+        let validatePrefix (args: Args<int>) =
             match args with
             | { Prefix = p; Limit = l } when p.Length < l ->
                 Ok args
@@ -70,7 +66,7 @@ module ArgValidation =
                     args.Prefix.Length
                     args.Limit)
 
-        let validateFileExists (args:Args<int>) =
+        let validateFileExists (args: Args<int>) =
             match args with
             | { Args.Limit = _; Args.File = f } ->
                 match f |> File.Exists with
@@ -96,18 +92,18 @@ module Io =
             | e -> Error $"Unexpectedly could not read \"{filePath}\": {e.Message}"
 
 module Quoting =
-    let processFileText (args:Args<int>) (text:string) =
+    let processFileText (args: Args<int>) (text: string) =
         let enquoten prefix text =
             text |> sprintf "%s%s" prefix
 
-        let splitLine fullLine (args:Args<int>) =
-            let finalSpaceIndex (text:string) (indexCeiling:int) =
+        let splitLine fullLine (args: Args<int>) =
+            let finalSpaceIndex (text: string) (indexCeiling:int) =
                 let index = text.LastIndexOf(" ", indexCeiling) // Searches backwards
                 match text.LastIndexOf(" ", indexCeiling) with
                 | -1 -> indexCeiling - 1
                 | _  -> index
 
-            let rec loop acc (linePart:string) limit =
+            let rec loop acc (linePart: string) limit =
                 match linePart.Length with
                 | l when l <= limit ->
                     acc @ [linePart]
